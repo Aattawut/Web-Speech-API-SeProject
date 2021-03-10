7a1fe76893078a8e3c0db8f621d26ba60ff4884f
Aattawut Nutlamyong <Aattawut.field@gmail.com>
2021-03-11 04:32:01 +0700

    29

--- js/main.js
+++ js/main.js
@@ -2,21 +2,29 @@ let mic = document.getElementById("mic");
 let chatareamain = document.querySelector('.chatarea-main');
 let chatareaouter = document.querySelector('.chatarea-outer');
 
-let intro = ["Hello, I am Chitti", "Hi, I am a Robo", "Hello, My name is Chitti"];
-let help = ["How may i assist you?","How can i help you?","What i can do for you?"];
-let greetings = ["สวัสดีค่ะ", "สวัสดีค่ะ", "สวัสดีค่ะ", "สวัสดีค่ะ"];
-let hobbies = ["i love to talk with humans", "i like to make friends like you", "i like cooking"];
-let pizzas = ["which type of pizza do you like?", "i can make a pizza for you", "i would love to make a pizza for you", "would you like cheese pizza?"];
-let thank = ["Most welcome","Not an issue","Its my pleasure","Mention not"];
-let closing = ['Ok bye-bye','As you wish, bye take-care','Bye-bye, see you soon..']
+var myWindow;
+let penrai = ["เเล้ววันนี้คนไข้มีอาการอะไรมาครับ", "เเล้ววันนี้คนไข้มีอาการอะไรมาครับ", "เเล้ววันนี้คนไข้มีอาการอะไรมาครับ"];
+let podhua = ["ปวดมากี่วันแล้วครับ","ปวดมากี่วันแล้ว","ปวดมากี่วันแล้ว"];
+let wan = ["คนไข้ วัดอุณภูมิ หรือยังครับ", "คนไข้ วัดอุณภูมิ หรือยังครับ", "คนไข้ วัดอุณภูมิ หรือยังครับ", "คนไข้ วัดอุณภูมิ หรือยังครับ"];
+let asktemp = ["เท่าไหร่ครับ", "เท่าไหร่ครับ", "เท่าไหร่ครับ"];
+let Jeabchor = ["เจ็บคอมั้ยครับ", "เจ็บคอมั้ยครับ", "เจ็บคอมั้ยครับ", "เจ็บคอมั้ยครับ"];
+let thank = ["เดี๋ยวเชิญเข้าห้องตรวจที่ 2 ครับ","เดี๋ยวเชิญเข้าห้องตรวจที่ 3 ครับ","เดี๋ยวเชิญเข้าห้องตรวจที่ 4 ครับ","เดี๋ยวเชิญเข้าห้องตรวจที่ 5 ครับ"];
+let closing = ['ใช่ครับ','ใช่ค่ะ'];
 
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-recognition.continuous = true;
 recognition.lang = 'th-TH';
 recognition.interimResults = false;
-recognition.maxAlternatives = 1;
+recognition.continuous = false;
+
+const sprc = window.SpeechRecognition || window.webkitSpeechRecognition;
+const rec = new SpeechRecognition();
+rec.lang = 'th-TH';
+rec.interimResults = false;
+rec.continuous = true;
+rec.maxAlternatives = 1;
+//rec.start(); //มันจะฟังเสียงบอทพูด ฟังครั้งแรก ต่อไปต้องกด
 
 function showusermsg(usermsg){
     let output = '';
@@ -32,51 +40,82 @@ function showchatbotmsg(chatbotmsg){
     return chatareaouter;
 }
 
-    
+
 function chatbotvoice(message){
-    
     var speech = new SpeechSynthesisUtterance();
     
-    speech.text = "Hi";
+    
 
-    
-    
-    if(message.includes('who are you')){
-        let finalresult = intro[Math.floor(Math.random() * intro.length)];
+    if(message.includes('ผมชื่อ')){
+        let finalresult = penrai[Math.floor(Math.random() * penrai.length)];
         speech.text = finalresult;
+    }
+    if(message.includes( 'ฉันชื่อ')){
+        let finalresult = penrai[Math.floor(Math.random() * penrai.length)];
         
-    }
-    if(message.includes('who are you')){
-        let finalresult = intro[Math.floor(Math.random() * intro.length)];
         speech.text = finalresult;
     }
-    if(message.includes('fine')){
-        let finalresult = help[Math.floor(Math.random() * help.length)];
+    if(message.includes('ปวดหัว' )){
+        let finalresult = podhua[Math.floor(Math.random() * podhua.length)];
         speech.text = finalresult;
     }
-    if(message.includes('สวัสดีครับ' || 'Hello')){
-        let finalresult = greetings[Math.floor(Math.random() * greetings.length)];
+    if(message.includes('หลายวันแล้ว')){
+        let finalresult = wan[Math.floor(Math.random() * wan.length)];
         speech.text = finalresult;
     }
-    if(message.includes('tell me something about you' || 'tell me something about your hobbies')){
-        let finalresult = hobbies[Math.floor(Math.random() * hobbies.length)];
+    if(message.includes('3 วันแล้ว')){
+        let finalresult = wan[Math.floor(Math.random() * wan.length)];
         speech.text = finalresult;
     }
-    if(message.includes('pizza')){
-        let finalresult = pizzas[Math.floor(Math.random() * pizzas.length)];
+    if(message.includes('วัดอุณหภูมิแล้ว')){
+        let finalresult = asktemp[Math.floor(Math.random() * asktemp.length)];
         speech.text = finalresult;
     }
-    if(message.includes('thank you' || 'thank you so much')){
+    if(message.includes('36.5')){
+        let finalresult = Jeabchor[Math.floor(Math.random() * Jeabchor.length)];
+        speech.text = finalresult;
+    }
+
+    if(message.includes('องศา')){
+        let finalresult = Jeabchor[Math.floor(Math.random() * Jeabchor.length)];
+        speech.text = finalresult;
+    }
+    
+    if(message.includes('เจ็บคอ' )){
         let finalresult = thank[Math.floor(Math.random() * thank.length)];
         speech.text = finalresult;
     }
-    if(message.includes('talk to you' || 'talk')){
+    if(message.includes('ไปทางไหน')){
+        
+        window.open("https://www.google.co.th/maps/place/%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B8%9E%E0%B8%A2%E0%B8%B2%E0%B8%9A%E0%B8%B2%E0%B8%A5%E0%B8%AA%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%AA%E0%B8%A3%E0%B8%B4%E0%B8%A1%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%9A%E0%B8%B2%E0%B8%87%E0%B8%88%E0%B8%B2%E0%B8%81/@10.7719059,99.3954974,17z/data=!3m1!4b1!4m5!3m4!1s0x30ff4642569fa129:0x26d5de083815eb08!8m2!3d10.7719006!4d99.3976861");
+    }
+    if(message.includes('โรงพยาบาลส่งเสริมสุขภาพตำบล')){
         let finalresult = closing[Math.floor(Math.random() * closing.length)];
         speech.text = finalresult;
+        window.open("http://gishealth.moph.go.th/pcu/admin/pcu.php?code=04358");
     }
+    if(message.includes('หยุดการทำงาน')){
+       
+            myWindow.close();
+          
+    }
+    if(message.includes('เริ่มใหม่')){
+        location.reload();  
+    }
+  
+    if(message.includes('ไปยังหน้าจดโน้ต')){
+        window.close();
+        window.open("https://healthhelpervoicebot.netlify.app/speechtotext.html");
+    }
+
+    
+    speechSynthesis.speak(speech);
     speech.lang = 'th-TH';
     window.speechSynthesis.speak(speech);
     chatareamain.appendChild(showchatbotmsg(speech.text));
+
+    scrollWin();
+    
 }
 
 recognition.onresult=function(e){
@@ -88,9 +127,14 @@ recognition.onresult=function(e){
 }
 recognition.onend=function(){
     mic.style.background="#ff3b3b";
+    
 }
+
 mic.addEventListener("click", function(){
     mic.style.background='#39c81f';
     recognition.start();
     console.log("Activated");
 })
+function scrollWin() {
+    window.scrollTo(0,document.body.scrollHeight);
+  }
